2dbd86e423f540a85473d0477e6d94c4a61fb037
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2019-02-27 19:41:27 -0300

    Added audio clip when a block is destroyed.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] AudioClip breakSound;    
+
+    private void OnCollisionEnter2D(Collision2D collision) //collision = Nome do objeto que colidiu com o Bloco
     {
-        //collision = Nome do objeto que colidiu com o Bloco
+        AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position); //Escolhemos a Camera pois assim o volume do audio será alto
         Destroy(gameObject);
     }
 }
