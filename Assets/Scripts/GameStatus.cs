d5720817053ba29b3450194c48433ffe89241225
Michael Maia <michaelmdasilva@yahoo.com.br>
2019-05-01 18:45:59 -0300

    AddToScore() method added.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GameStatus : MonoBehaviour {
 
+    //Config Parameters
     [Range(0.1f,10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //State Variables
+    [SerializeField] int currentScore = 0;
 	
 	// Update is called once per frame
 	void Update () {
         Time.timeScale = gameSpeed;
 	}
+
+    public void AddToScore(){
+        currentScore += pointsPerBlockDestroyed;
+    }
+
 }
